955fee91c5ae37bf4f5e3907b53243f20384edae
C21403052 <C21403052@mytudublin.ie>
2023-09-29 12:03:32 +0100

    This file prints This is another string!

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String [] args){
-        System.out.printl("Welcome");
+        System.out.printl("This is another string!");
 	int i;
 	for(i=0;i<10;i++){
 		System.out.println(i+1);
