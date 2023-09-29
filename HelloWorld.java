b175362131d5b28e7d2f64976f103b4906850418
C21403052 <C21403052@mytudublin.ie>
2023-09-29 11:57:30 +0100

    This string prints "THis is another string"

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String [] args){
-        System.out.printl("Hello");
+        System.out.printl("This is a new string");
 	int i;
 	for(i=0;i<10;i++){
 		System.out.println(i+1);
