1548eea75b9028dd82f932ff575803ae10edb07e
C21403052 <C21403052@mytudublin.ie>
2023-09-29 11:38:54 +0100

    Please enter the commit message for your changes. Lines starting

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,10 @@
 public class HelloWorld {
     public static void main(String [] args){
         System.out.printl("Hello");
+	int i;
+	for(i=0;i<10;i++){
+		System.out.println(i+1);
+	}
     }
     
 }
