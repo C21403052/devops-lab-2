e6fe7d5419071a33063507f76cf2a89f2d8b885b
C21403052 <C21403052@mytudublin.ie>
2023-09-29 12:06:44 +0100

    This file says Welcome!

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String [] args){
-        System.out.printl("Welcome");
+        System.out.printl("Welcome!");
 	int i;
 	for(i=0;i<10;i++){
 		System.out.println(i+1);
